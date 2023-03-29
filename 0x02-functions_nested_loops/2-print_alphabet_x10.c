4d0998df6ed209e646b45a4053e5b270e9306e03
Amany74 <amanybusiness74@gmail.com>
2023-03-29 15:07:57 -0700

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_alphabet - a function to print alphabet
+ * in lowecase using putchar only twice 
+ *
+ * return : void 
+**/
+
+void print_alphabet(void)
+{
+	char i;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+}
